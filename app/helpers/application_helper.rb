f91dc59d12a20259f2c1b21048fa2d442080e170
Will Feng <willxfeng@gmail.com>
2016-01-27 13:17:14 -0500

    js randomize background image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-  def randomized_background_image
-    images = [
-      asset_path("background1.jpg"),
-      asset_path("background2.jpg"),
-      asset_path("background3.jpg")
-    ]
-    puts "-----------------------#{images[0]}"
-    images.sample
-  end
 end
