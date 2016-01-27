8d615af25b581763daf6bb4a008fbb3bfd500ccd
Will Feng <willxfeng@gmail.com>
2016-01-27 13:48:53 -0500

    c

--- app/assets/javascripts/randomize_bg_img_dev.js
+++ app/assets/javascripts/randomize_bg_img_dev.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-  $('body').css('background-image', bgImage())
-});
-
-var bgImage = function() {
-  root = 'url("http://localhost:3000'
-  bgImages = [
-    root + '/assets/background1.jpg")',
-    root + '/assets/background2.jpg")',
-    root + '/assets/background3.jpg")'
-  ]
-  return bgImages[Math.floor(Math.random() * bgImages.length)]
-}
+// $(document).ready(function() {
+//   $('body').css('background-image', bgImage())
+// });
+//
+// var bgImage = function() {
+//   root = 'url("http://localhost:3000'
+//   bgImages = [
+//     root + '/assets/background1.jpg")',
+//     root + '/assets/background2.jpg")',
+//     root + '/assets/background3.jpg")'
+//   ]
+//   return bgImages[Math.floor(Math.random() * bgImages.length)]
+// }
