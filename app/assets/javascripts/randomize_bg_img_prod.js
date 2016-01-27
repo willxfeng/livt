4a508dbb1d2c7bc7cfb295b5bafc52b5cd43bd16
Will Feng <willxfeng@gmail.com>
2016-01-27 13:22:23 -0500

    js random background production

---
+++ app/assets/javascripts/randomize_bg_img_prod.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('body').css('background-image', bgImage())
+});
+
+var bgImage = function() {
+  root = 'url("https://livt.herokuapp.com'
+  bgImages = [
+    root + '/assets/background1.jpg")',
+    root + '/assets/background2.jpg")',
+    root + '/assets/background3.jpg")'
+  ]
+  return bgImages[Math.floor(Math.random() * bgImages.length)]
+}
