7f958e66e80a91f9f3ab33fe1b942fad9fbf46e8
Will Feng <willxfeng@gmail.com>
2016-01-27 13:28:46 -0500

    dfd

--- app/assets/javascripts/randomize_bg_img_prod.js
+++ app/assets/javascripts/randomize_bg_img_prod.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-  $('body').css('background-image', bgImage())
-});
-
-var bgImage = function() {
-  root = 'url("https://livt.herokuapp.com'
-  bgImages = [
-    root + '/assets/background1.jpg")',
-    root + '/assets/background2.jpg")',
-    root + '/assets/background3.jpg")'
-  ]
-  return bgImages[Math.floor(Math.random() * bgImages.length)]
-}
+// $(document).ready(function() {
+//   $('body').css('background-image', bgImage())
+// });
+//
+// var bgImage = function() {
+//   root = 'url("https://livt.herokuapp.com'
+//   bgImages = [
+//     root + '/assets/background1.jpg")',
+//     root + '/assets/background2.jpg")',
+//     root + '/assets/background3.jpg")'
+//   ]
+//   return bgImages[Math.floor(Math.random() * bgImages.length)]
+// }
